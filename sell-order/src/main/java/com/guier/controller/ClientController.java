233af98bc24f0791567a5cb439960b94b9521a15
wangyigui <466007639@qq.com>
2019-03-29 15:32:18 +0800

    'client获取配置'

--- sell-order/src/main/java/com/guier/controller/ClientController.java
+++ sell-order/src/main/java/com/guier/controller/ClientController.java
@@ -2,8 +2,10 @@ package com.guier.controller;
 
 import com.guier.feign.Orderfeign;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -11,6 +13,8 @@ import java.util.List;
 
 @RestController
 public class ClientController {
+    @Value("${qiqi}")
+    private String name;
 
     @Autowired
     private Orderfeign orderfeign;
@@ -24,6 +28,11 @@ public class ClientController {
     public List listForOrder() {
         List list = orderfeign.listForOrder(Arrays.asList("11"));
         return list;
+    }
+
+    @RequestMapping("/name")
+    public String getName() {
+        return name;
     }
 
 }
--- sell-order/src/main/java/com/guier/feign/Orderfeign.java
+++ sell-order/src/main/java/com/guier/feign/Orderfeign.java
@@ -1,12 +1,13 @@
 package com.guier.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
-
+@Component
 @FeignClient(name = "sell-product")
 public interface Orderfeign {
     @GetMapping("/msg")
